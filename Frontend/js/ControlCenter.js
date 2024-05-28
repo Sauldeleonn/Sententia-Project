443c6b4232f4adaaddb2ca753b7fb5b22221e9cc
Sauldeleonn <sauldeleonn@hotmail.com>
2024-05-27 19:14:36 -0600

    Correcciones Front (Pantalla Admin) y Doc Tecnica

---
+++ Frontend/js/ControlCenter.js
@@ -0,0 +1,4 @@
+function showContent(section) {
+    document.getElementById('header-box').innerText = section;
+    document.getElementById('content-box').innerText = 'Content for ' + section + ' will be displayed here.';
+}
